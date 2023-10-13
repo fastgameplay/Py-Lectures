aba5f4991e67b895f403526d2eb8f9d71ca42992
fastgameplay <61421145+fastgameplay@users.noreply.github.com>
2023-10-13 20:20:15 +0400

    Character

---
+++ MiniQuiz/character.py
@@ -0,0 +1,36 @@
+import random
+
+class Character:
+    def __init__(self, name, health, attack_power):
+        self._name = name
+        self.health = health
+        self._attack_power = attack_power
+
+    @property
+    def name(self):
+        return self._name
+
+
+    @property
+    def attack_power(self):
+        return self._attack_power
+    @property
+    def health(self):
+        return self._health
+
+    @health.setter
+    def health(self, value):
+        if value >= 0:
+            self._health = value
+        else:
+            print("Health cannot be negative. Setting health to 0.")
+            self._health = 0
+
+    def attack(self, opponent):
+        damage = random.randint(1, self.attack_power)
+        opponent.health -= damage
+        print(f"{self.name} attacks {opponent.name} for {damage} damage!")
+
+    def __str__(self):
+        return f"{self.name} (Health: {self.health}, Attack Power: {self.attack_power})"
+    
